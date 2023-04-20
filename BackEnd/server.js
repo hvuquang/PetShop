b9d16d54dd03e64b1faf7e28f951bccd58e8e7c0
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2023-04-20 20:38:44 +0700

    update server , create middleware upload image

---
+++ BackEnd/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.mongodb_connect_url).then(()=>console.log("Connected Database"))
+
+app.listen(8000,()=>{
+    console.log('Server running on port 8000');
+})
