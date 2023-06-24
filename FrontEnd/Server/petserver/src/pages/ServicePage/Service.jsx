12f3adb609edad8da453a87df19c50b364943cb9
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-06-24 14:32:13 +0700

    call api readAllService

--- FrontEnd/Server/petserver/src/pages/ServicePage/Service.jsx
+++ FrontEnd/Server/petserver/src/pages/ServicePage/Service.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Service.css";
 import addicon from "../../images/pet-icon.png";
 import Card from "../../components/Card/Card";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
 import AddService from "../AddService/AddService";
-
+import axios from "axios";
 function Service() {
   const [modalState, setmodalState] = useState(false);
 
@@ -12,6 +12,19 @@ function Service() {
     event.preventDefault();
     setmodalState(!modalState);
   }
+
+  useEffect(
+    () => {
+      axios.get("http://localhost:8000/v1/service/readAllService").then(
+        (res) => {
+          console.log(res.data);
+        },
+        (err) => {
+          console.log(err);
+        }
+      )},
+    []
+  );
 
   return (
     <div className="home-section">
