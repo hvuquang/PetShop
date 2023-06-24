7f4634896c8234f13b0a77f820ead4f0103e9d02
Numb246 <113155975+hvuquang@users.noreply.github.com>
2023-06-24 16:18:17 +0700

    update UI
    - add price
    - service description

--- FrontEnd/Server/petserver/src/components/ServiceType/ServiceType.jsx
+++ FrontEnd/Server/petserver/src/components/ServiceType/ServiceType.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ServiceType.css";
-import { boarding } from "../../images/Boarding.png";
-import { dogwalking } from "../../images/dog-walking.png";
-import { petcare } from "../../images/petcare.png";
-import { housesitting } from "../../images/house-sitting.png";
+import  boarding from "../../images/Boarding.png";
+import  dogwalking  from "../../images/dog-walking.png";
+import  petcare  from "../../images/petcare.png";
+import  housesitting from "../../images/house-sitting.png";
 import petsupply from "../../images/pet-supplies.png";
 
 export default function ServiceType(props) {
   const clickAction = props.action
   const isClicked = props.isClicked
+  const serviceType = props.serviceType
+  const [type, setType] = useState("Service Type")
+  const [imgSrc, setImgSrc] = useState(petsupply)
+
+  useEffect(() => {
+    if (serviceType === "housesitting") {
+      setType("House Sitting")
+      setImgSrc(housesitting)
+    }
+    else if (serviceType === "petwalking") {
+      setType("Pet Walking")
+      setImgSrc(dogwalking)
+    }
+    else if (serviceType === "petspa") {
+      setType("Pet Spa")
+      setImgSrc(petsupply)
+    }
+    else if (serviceType === "boarding") {
+      setType("Boarding")
+      setImgSrc(boarding)
+    }
+    else if (serviceType === "petdaycare") {
+      setType("Day Care")
+      setImgSrc(petcare)
+    }
+  }, [])
+  // serviceSelectedType()
   return (
     <div
       className={`service-type-container ${
@@ -16,8 +43,8 @@ export default function ServiceType(props) {
       }`
     } onClick={clickAction}
     >
-      <img src={petsupply} alt="a service for pet" />
-      <p>Service Type</p>
+      <img src={imgSrc} alt="a service for pet" />
+      <p>{type}</p>
     </div>
   );
 }
--- FrontEnd/Server/petserver/src/pages/AddService/AddService.jsx
+++ FrontEnd/Server/petserver/src/pages/AddService/AddService.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Uploader from "../../components/Uploader/Uploader";
 import ServiceType from "../../components/ServiceType/ServiceType";
@@ -10,18 +10,25 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function AddService(props) {
   const [img, setImg] = useState();
+  const [serviceDescription, setServiceDescription] = useState("")
+  let spaDes = "Spa cho thú cưng"
+  let housesittingDes = "Chăm sóc thú cưng tại nhà riêng"
+  let petwalkingDes = "Đi bộ với thú cưng"
+  let boardingDes = "Nhận chăm sóc thú cưng tại cửa hàng"
+  let daycareDes = "Chăm sóc thú cưng theo giờ tron ngày"
   //click vào dịch vụ
   const [serviceClicked, setServiceClicked] = useState(false);
   const [serviceClicked1, setServiceClicked1] = useState(false);
   const [serviceClicked2, setServiceClicked2] = useState(false);
   const [serviceClicked3, setServiceClicked3] = useState(false);
   const [serviceClicked4, setServiceClicked4] = useState(false);
+  const [price, setPrice] = useState(0)
+  let servicePrice = 300000
   //cài đặt cho DatePicker React
   const [startDate, setStartDate] = useState(new Date());
   //cài đặt popup
   const modalState = props.toggle;
   const action = props.action;
-
   let food = props.food;
   let updateFood = props.addfood;
   const showFood = props.showFood;
@@ -53,22 +60,66 @@ function AddService(props) {
 
   const updateService = () => {
     setServiceClicked(!serviceClicked)
+    if (serviceClicked === false) {
+      setPrice(price + servicePrice)
+      setServiceDescription((serviceDescription + "\n" + housesittingDes).trimStart())
+    }
+    else if (serviceClicked === true) {
+      setPrice(price - servicePrice)
+      // setServiceDescription(serviceDescription - housesittingDes)
+    }
+    console.log(price)
+    console.log(serviceDescription)
   }
   
   const updateService1 = () => {
     setServiceClicked1(!serviceClicked1)
+    if (serviceClicked1 === false) {
+      setPrice(price + servicePrice)
+      setServiceDescription((serviceDescription + "\n" + petwalkingDes).trimStart())
+    }
+    else if (serviceClicked1 === true) {
+      setPrice(price - servicePrice)
+    }
   }
 
   const updateService2 = () => {
     setServiceClicked2(!serviceClicked2)
+    if (serviceClicked2 === false) {
+      setPrice(price + servicePrice)
+      setServiceDescription((serviceDescription + "\n" + spaDes).trimStart())
+    }
+    else if (serviceClicked2 === true) {
+      setPrice(price - servicePrice)
+    }
   }
 
   const updateService3 = () => {
     setServiceClicked3(!serviceClicked3)
+    if (serviceClicked3 === false) {
+      setPrice(price + servicePrice)
+      setServiceDescription((serviceDescription + "\n" + boardingDes).trimStart())
+    }
+    else if (serviceClicked3 === true) {
+      setPrice(price - servicePrice)
+    }
   }
 
   const updateService4 = () => {
     setServiceClicked4(!serviceClicked4)
+    if (serviceClicked4 === false) {
+      setPrice(price + servicePrice)
+      setServiceDescription((serviceDescription + "\n" + daycareDes).trimStart())
+    }
+    else if (serviceClicked4 === true) {
+      setPrice(price - servicePrice)
+    }
+  }
+
+  const handlerChangeDes = (e) => 
+  {
+    let description = document.getElementById("serviceDescription").value
+    setServiceDescription(description)
   }
 
   return (
@@ -77,15 +128,15 @@ function AddService(props) {
       <div id="addpet-section">
         <h1>Thêm dịch vụ</h1>
         <form>
-          <div className="food-form-section">
+          <div className="food-form-section" id="service-form-header">
             <div className="addpet-title">
               Chọn loại dịch vụ
               <div className="service-container">
-                <ServiceType isClicked={serviceClicked} action={updateService}/>
-                <ServiceType isClicked={serviceClicked1} action={updateService1}/>
-                <ServiceType isClicked={serviceClicked2} action={updateService2}/>
-                <ServiceType isClicked={serviceClicked3} action={updateService3}/>
-                <ServiceType isClicked={serviceClicked4} action={updateService4}/>
+                <ServiceType isClicked={serviceClicked} action={updateService} serviceType="housesitting"/>
+                <ServiceType isClicked={serviceClicked1} action={updateService1} serviceType="petwalking"/>
+                <ServiceType isClicked={serviceClicked2} action={updateService2} serviceType="petspa"/>
+                <ServiceType isClicked={serviceClicked3} action={updateService3} serviceType="boarding"/>
+                <ServiceType isClicked={serviceClicked4} action={updateService4} serviceType="petdaycare"/>
               </div>
             </div>
             <br />
@@ -98,6 +149,21 @@ function AddService(props) {
                 // onChange={updateFood}
                 className="addpet-input"
               ></input>
+            </div>
+            <br />
+            <div className="addpet-title">
+              Chi tiết dịch vụ?
+              <textarea
+                name="serviceDescription"
+                // value={food.foodDescription}
+                // onChange={updateFood}
+                onChange={handlerChangeDes}
+                className="addpet-input"
+                // inputMode
+                // value={serviceDescription}
+                value={serviceDescription}
+                id="serviceDescription"
+              ></textarea>
             </div>
             <br />
             <div className="datepicker-container">
@@ -153,6 +219,7 @@ function AddService(props) {
                   // value={food.foodPrice}
                   // onChange={updateFood}
                   className="addpet-input"
+                  placeholder={price}
                 ></input>
               </div>
             </div>
